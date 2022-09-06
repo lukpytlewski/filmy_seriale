cc1eba59083f00f83214cf5891eba2cd0681d2d7
Lukasz Pytlewski <lukpytlewski@yahoo.com>
2022-09-06 21:48:37 +0200

    Wprowadzenie sryliarda poprawek

--- filmy_seriale.py
+++ filmy_seriale.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Movies:
     def __init__(self, tytul, rok, gatunek, odtworzenia):
         self.tytul = tytul
@@ -11,6 +14,9 @@ class Movies:
     def __str__(self):
         return f"{self.tytul} ({self.rok})"
 
+    def __repr__(self):
+        return f"{self.tytul} ({self.rok})"
+
 
 class Series(Movies):
     def __init__(self, numer_odcinka, numer_sezonu, *args, **kwargs):
@@ -18,49 +24,46 @@ class Series(Movies):
         self.numer_odcinka = numer_odcinka
         self.numer_sezonu = numer_sezonu
 
-    def play(self, step=1):
-        self.odtworzenia += step
-
     def __str__(self):
-        return f"{self.tytul} S%.2iE%.2i" % (
-            int(self.numer_sezonu),
-            int(self.numer_odcinka),
-        )
+        return f"{self.tytul} S{self.numer_sezonu:02}E{self.numer_odcinka:02}"
+
+    def __repr__(self):
+        return f"{self.tytul} S{self.numer_sezonu:02}E{self.numer_odcinka:02}"
 
 
 def get_movies(movies_and_series_list):
     movies_list = []
-    for i in range(len(movies_and_series_list)):
-        if isinstance(movies_and_series_list[i], Series) == True:
-            pass
-        else:
-            movies_list.append(movies_and_series_list[i])
-    sorted(movies_list, key=lambda movies: movies.tytul)
+    for elem in movies_and_series_list:
+        if type(elem) == Movies:
+            movies_list.append(elem)
+    movies_list.sort(key=lambda movies: movies.tytul)
     return movies_list
 
 
 def get_series(movies_and_series_list):
     series_list = []
-    for i in range(len(movies_and_series_list)):
-        if isinstance(movies_and_series_list[i], Series) == True:
-            series_list.append(movies_and_series_list[i])
-        else:
-            pass
-    sorted(series_list, key=lambda series: series.tytul)
+    for elem in movies_and_series_list:
+        if type(elem) == Series:
+            series_list.append(elem)
+    series_list.sort(key=lambda series: series.tytul)
     return series_list
 
 
+def lista_pozycji(lst_filmow_i_seriali, type):
+    if type == 1:
+        return get_movies(lst_filmow_i_seriali)
+    elif type == 2:
+        return get_series(lst_filmow_i_seriali)
+
+
 def search(title, lista_filmow_i_seriali):
-    for i in range(len(lista_filmow_i_seriali)):
-        if title == lista_filmow_i_seriali[i].tytul:
+    is_found = False
+    for elem in lista_filmow_i_seriali:
+        if title == elem.tytul:
             print("Mamy tę pozycję w bibliotece")
-            break
-        else:
-            if i == len(lista_filmow_i_seriali) - 1:
-                print("Nie ma takiego tytułu w bibliotece")
-
-
-import random
+            is_found = True
+    if is_found == False:
+        print("Nie ma takiego tytułu w bibliotece")
 
 
 def generate_views(lista_filmow_i_seriali):
@@ -78,52 +81,63 @@ def generate_views_x10(lista_filmow_i_seriali):
 
 
 def top_titles(lista_filmow_i_seriali, n, type):
-    if n >= len(lista_filmow_i_seriali):
-        n = len(lista_filmow_i_seriali) - 1
+    top_list = []
+    lst = []
     if type == 1:
-        top_movies_list = []
-        sorted(
-            get_movies(lista_filmow_i_seriali), key=lambda movies: movies.odtworzenia
-        )
-        for i in range(n):
-            top_movies_list.append(get_movies(lista_filmow_i_seriali)[i].tytul)
-        return top_movies_list
+        lst = get_movies(lista_filmow_i_seriali)
     elif type == 2:
-        top_series_list = []
-        sorted(
-            get_series(lista_filmow_i_seriali), key=lambda series: series.odtworzenia
-        )
-        for i in range(n):
-            top_series_list.append(get_series(lista_filmow_i_seriali)[i].tytul)
-        return top_series_list
+        lst = get_series(lista_filmow_i_seriali)
+    lst.sort(key=lambda list: list.odtworzenia, reverse=True)
+    for i in range(n):
+        top_list.append(lst[i])
+    return top_list
 
 
 if __name__ == "__main__":
 
     filmy_i_seriale = [
         Movies(tytul="Harold and Maude", rok=1984, gatunek="horror", odtworzenia=0),
+        Movies(tytul="Odyseja kosmiczna", rok=1969, gatunek="sci-fi", odtworzenia=0),
+        Movies(tytul="Abrakdabra", rok=2011, gatunek="sci-fi", odtworzenia=0),
         Series(
-            tytul="Allo allo",
+            tytul="Hallo allo",
             rok=1984,
             gatunek="horror",
             odtworzenia=0,
             numer_odcinka=3,
             numer_sezonu=4,
         ),
+        Series(
+            tytul="Breaking bad",
+            rok=2000,
+            gatunek="horror",
+            odtworzenia=0,
+            numer_odcinka=1,
+            numer_sezonu=5,
+        ),
+        Series(
+            tytul="True detective",
+            rok=2001,
+            gatunek="horror",
+            odtworzenia=0,
+            numer_odcinka=2,
+            numer_sezonu=2,
+        ),
     ]
 
-    print(filmy_i_seriale[0], filmy_i_seriale[1])
+    print(filmy_i_seriale)
+    print()
 
-    for i in range(len(get_movies(filmy_i_seriale))):
-        print(get_movies(filmy_i_seriale)[i])
+    print(lista_pozycji(filmy_i_seriale, 1))
+    print(lista_pozycji(filmy_i_seriale, 2))
+    print()
 
-    for i in range(len(get_series(filmy_i_seriale))):
-        print(get_series(filmy_i_seriale)[i])
-
-    search("Allo allo", filmy_i_seriale)
+    search("Hallo allo", filmy_i_seriale)
+    print()
 
     generate_views(filmy_i_seriale)
 
     generate_views_x10(filmy_i_seriale)
+    print()
 
     print(top_titles(filmy_i_seriale, 3, 2))
